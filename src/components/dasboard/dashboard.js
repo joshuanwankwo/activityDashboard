3d0d0fb82bd4baaf52af91280126772e84126f22
Joshua Nwankwo <systemblakes@gmail.com>
2019-10-20 23:04:07 +0100

    The login page was conflicting with the dashboard so I removed it

--- src/components/dasboard/dashboard.js
+++ src/components/dasboard/dashboard.js
@@ -3,7 +3,6 @@ import "./dashboard.css"
 import SideMenu from "../sidemenu/sidemenu"
 import Board from "../board/board"
 import NavBar from "../navbar/navbar"
-import ApplicForm from "../login/Login"
 
 
  class Dashboard extends React.Component{
